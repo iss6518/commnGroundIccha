8f88054f1658f901f8a4f66368f2c580a4ec452b
Iccha02 <iccha02@gmail.com>
2024-02-08 20:17:49 -0500

    All FE changes from 2/8/24 lecture

--- src/Components/Games/games.jsx
+++ src/Components/Games/games.jsx
@@ -1,22 +1,74 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import propTypes from 'prop-types';
 import axios from 'axios';
 
+function AddGameForm({ setError, fetchGames, cancel, visible }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState(0);
+
+  const changeName = (event) => { setName(event.target.value); };
+  const changeNumber = (event) => { setNumber(event.target.value); };
+
+  const addGame = (event) => {
+    event.preventDefault();
+    axios.post('http://localhost:8000/users', { name: name, numPlayers: number }) // sends a post request
+    .then(() => {
+      setError('');
+      fetchGames();
+    })
+    .catch((error) => {
+      setError(error.response.data.message);
+    });
+  };
+  
+  // if (!visible) return null;
+  return(
+    <form>
+        <label htmlFor="name"> 
+          Name
+        </label>
+        <input type="text" id="name" value={name} onChange={changeName}/>
+        <label htmlFor="name"> 
+          Number
+        </label>
+        <input type="number" id="number" value={number} onChange={changeNumber}/>
+        <button type="submit" onClick={addGame}>Submit</button>
+        <button type="button" onClick={cancel}>Cancel</button>
+    </form>
+  );
+}
+
+// for adding cancel/visible
+// **** TODO need to figure this out
+AddGameForm.propTypes = {
+  visible: propTypes.bool.isRequired,
+  cancel: propTypes.func.isRequired,
+  fetchGames: propTypes.func.isRequired,
+  setError: propTypes.func.isRequired,
+};
+
 function Games() {
-  const [error, setError] = useState('Initial message');
+  const [error, setError] = useState('');
   const [games, setGames] = useState([]);
+  // const [addingGame, setAddingGame] = useState(true);
+
+  const fetchGames = () => {
+    axios.get('http://localhost:8000/users')
+    .then((response) => {
+      const gamesObject = response.data.Data;
+      const keys = Object.keys(gamesObject);
+      const gamesArray = keys.map((key) => gamesObject[key]);
+      setGames(gamesArray);
+      console.log(response);
+    }) // something good
+    .catch(() => { setError('Something went wrong'); }); //something bad
+  };
+
+  // const showAddGameForm = () => { setAddingGame(true); };
+  // const hideAddGameForm = () => { setAddingGame(false); };
 
   useEffect(
-    () => {
-      // call to bkend to get additional info
-      axios.get('http://localhost:8000/users')
-        .then((response) => {
-          const gamesObject = response.data.Data;
-          const keys = Object.keys(gamesObject);
-          const gamesArray = keys.map((key) => gamesObject[key]);
-          setGames(gamesArray);
-        }) // something good
-        .catch(() => { setError('Something went wrong'); }); //something bad
-    },
+    fetchGames,
     [],
     // if THIS isempty meaning it'll only
     // be called on initial render of app
@@ -32,6 +84,11 @@ function Games() {
       {error}
       </div>
     )}
+    <AddGameForm 
+    setError={setError}
+    fetchGames={fetchGames}
+    // cancel={hideAddGameForm}
+    />
     {games.map((game) => (
       <div className="game-container">
         <h2>{game.user_name}</h2>
