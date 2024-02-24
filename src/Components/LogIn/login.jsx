e2888cc6df0809648f0ec3dffbafc1774f9d8038
Dareck <dba6398@nyu.edu>
2024-02-24 13:10:14 -0500

    added session managment for login

--- src/Components/LogIn/login.jsx
+++ src/Components/LogIn/login.jsx
@@ -3,18 +3,25 @@ import propTypes from 'prop-types';
 import axios from 'axios';
 // import { BACKEND_URL } from '../../constants';
 
+//import { useNavigate, Link } from 'react-router-dom';
+//import {useDispatch} from 'react-redux'
+
 const USERS_ENDPOINT = `http://127.0.0.1:8000/users`;
 // const USERS_ENDPOINT = `${BACKEND_URL}/users`;
 
 function LogInForm({ setError, fetchUser, cancel, visible }) {
+  // initial state is empty for both name and password
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
 
+  // functions to handle user inputs for username & password (triggered on state change) 
   const changeName = (event) => { setName(event.target.value); };
   const changePassword = (event) => { setPassword(event.target.value); };
 
+  // function to handle submit ("log in") button
   const logIn = (event) => {
-    event.preventDefault();
+    event.preventDefault(); // default when submitting form is to reload page & we don't want that
+
     axios.post(USERS_ENDPOINT, { name: name, password: password })
     .then(() => {
       setError('');
@@ -33,10 +40,12 @@ function LogInForm({ setError, fetchUser, cancel, visible }) {
           Name
         </label>
         <input type="text" id="name" value={name} onChange={changeName}/>
+
         <label htmlFor="name"> 
           Password
         </label>
         <input type="text" id="name" value={password} onChange={changePassword}/>
+
         <button type="submit" onClick={logIn}>LogIn</button>
         <button type="button" onClick={cancel}>Cancel</button>
     </form>
@@ -54,23 +63,18 @@ LogInForm.propTypes = {
 
 function Login() { //fetching from backend
   const [error, setError] = useState('');
-  const [games, setGames] = useState([]);
-  // const [addingGame, setAddingGame] = useState(true);
 
   const fetchUser = () => {
     axios.get(USERS_ENDPOINT)
     .then((response) => {
-      const gamesObject = response.data.Data;
-      const keys = Object.keys(gamesObject);
-      const gamesArray = keys.map((key) => gamesObject[key]);
-      setGames(gamesArray);
-      console.log(response);
+        if (response.data.accessToken) {
+            localStorage.setItem("user", JSON.stringify(response.data));
+          }
+          console.log(response.data);
+          // return response.data;
     }) // something good
     .catch(() => { setError('Something went wrong'); }); //something bad
   };
-
-  // const showAddGameForm = () => { setAddingGame(true); };
-  // const hideAddGameForm = () => { setAddingGame(false); };
 
   useEffect(
     fetchUser,
@@ -94,11 +98,6 @@ function Login() { //fetching from backend
     fetchUser={fetchUser}
     // cancel={hideAddGameForm}
     />
-    {games.map((game) => (
-      <div className="game-container">
-        <h2>{game.user_name}</h2>
-      </div>
-    ))}
   </div>
   );
 }
