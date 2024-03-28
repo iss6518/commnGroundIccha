6ce6007e983b99efb6e98fe95f7d9b6e4d01d52b
Kelvin Delarosa <kd2413@nyu.edu>
2024-03-28 11:01:01 -0400

    Adding LogIn Box Component

---
+++ src/Components/LoginBox.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+function LoginBox() {
+    return (
+      <div className="login-container">
+        <h2>Member Login</h2>
+        <form>
+          <div className="input-group">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
+          </div>
+          <div className="actions">
+            <button type="submit">Login</button>
+            <div className="links">
+              <a href="#">Forgot password?</a>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+  
+
+  
+export default LoginBox;
