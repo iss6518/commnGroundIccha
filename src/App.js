67f7c26f686ecb7dc7f143d95a014f22c785469b
Kelvin Delarosa <kd2413@nyu.edu>
2024-03-28 11:14:59 -0400

    Styling LoginBox

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ function App() {
     <Routes>
     <Route path="/" element={
           <>
+          <div className="welcome-container"></div>
             <h1>Welcome to CommonGround!</h1>
             <LoginBox />
           </>
