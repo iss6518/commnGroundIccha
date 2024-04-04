fa461bc15aa338e5d4eddffa856f703722e8c831
ProspectInc <rm5572@nyu.edu>
2024-04-04 19:17:34 -0400

    Update form_filler.py
    reversing changes

--- src/forms/form_filler.py
+++ src/forms/form_filler.py
@@ -3,7 +3,7 @@
 A utility for filling in a form in a notebook or
 from the command line.
 """
-FLD_NM = None
+FLD_NM = 'fld_nm'
 QSTN = 'question'
 DESCR = 'description'
 INSTRUCTIONS = 'instructions'
