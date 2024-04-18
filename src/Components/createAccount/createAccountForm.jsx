79e61466ef77c656076f07fe323e24921dfd2649
Dareck <dba6398@nyu.edu>
2024-04-18 14:15:24 -0400

    added an email and password field for createAccount

--- src/Components/createAccount/createAccountForm.jsx
+++ src/Components/createAccount/createAccountForm.jsx
@@ -4,6 +4,12 @@ import { BACKEND_URL } from '../../constants';
 
 const USERS_ENDPOINT = `${BACKEND_URL}/users`;
 
+/*
+TODO: 
+* need success message & clear out form once user clicks create account 
+* need to redirect to login page once user creates account
+*/
+
 function CreateAccountForm({ onSubmit }) {
 
   // initial state is empty for both name and password
@@ -11,8 +17,8 @@ function CreateAccountForm({ onSubmit }) {
   const [age, setAge] = useState(0);
   const [gender, setGender] = useState('');
   const [interests, setInterest] = useState('');
-  //const [email, setEmail] = useState('');
-  //const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [error, setError] = useState("");
 
   // functions to handle user inputs for username & password (triggered on state change) 
@@ -20,8 +26,8 @@ function CreateAccountForm({ onSubmit }) {
   const changeAge = (event) => { setAge(event.target.value); };
   const changeGender = (event) => { setGender(event.target.value); };
   const changeInterest = (event) => { setInterest(event.target.value); };
-  //const changeEmail = (event) => { setEmail(event.target.value); };
-  //const changePassword = (event) => { setPassword(event.target.value); };
+  const changeEmail = (event) => { setEmail(event.target.value); };
+  const changePassword = (event) => { setPassword(event.target.value); };
 
   
   const createAccount = async (data) => {
@@ -36,7 +42,7 @@ function CreateAccountForm({ onSubmit }) {
   // called when create account button is pressed
   const handleSubmit = (event) => {
     event.preventDefault();
-    createAccount({user_name, age, gender, interests});
+    createAccount({user_name, age, gender, interests, email, password});
   };
 
   return (
@@ -51,6 +57,16 @@ function CreateAccountForm({ onSubmit }) {
             value={user_name}
             onChange={changeName}
             name="user_name"
+            required
+          />
+
+          <label htmlFor="email">Email: </label>
+          <input
+            type="text"
+            id="email"
+            value={email}
+            onChange={changeEmail}
+            name="email"
             required
           />
 
@@ -80,6 +96,16 @@ function CreateAccountForm({ onSubmit }) {
             value={interests}
             onChange={changeInterest}
             name="interests"
+            required
+          />
+
+          <label htmlFor="password">Password: </label>
+          <input
+            type="text"
+            id="password"
+            value={password}
+            onChange={changePassword}
+            name="password"
             required
           />
 
