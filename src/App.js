7bb06f764866eb5846f7e523d3b7e9cd02cb0a86
ProspectInc <rm5572@nyu.edu>
2024-04-28 16:46:26 -0400

    Adding route from Home to login and createAccount

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import {
   BrowserRouter,
   Routes,
-  Route
+  Route,
+  useNavigate
 } from 'react-router-dom';
 import './App.css';
 
@@ -22,6 +23,37 @@ import Users from './Components/Users';
 import Login from './Components/LogIn';
 import CreateAccountForm from './Components/createAccount';
 import FormBuilder from './forms/formBuilder'; 
+
+
+function Home() {
+  const navigate = useNavigate(); // Setup navigate function
+
+  return (
+    <>
+      <div className="welcome-message">
+        <center><h1>Welcome to <span className="highlight">CommonGround!</span></h1></center>
+      </div>
+
+      <div className="homepage-container">
+        <div className="content-section">
+          <h2 className="headline"><span className="highlight">CommonGround</span> is a platform that connects people with shared interests and goals!</h2>
+          <p className="subtext">
+            Its versatility acts as a mentorship, hobby sharing, or partner matching tool
+          </p>
+          {/* Buttons for Login and Create Account */}
+          <div className="button-container">
+            <button onClick={() => navigate('/login')} className="link-button">Login</button>
+            <button onClick={() => navigate('/createaccount')} className="link-button">Create Account</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="image-container">
+        <img src="https://media.istockphoto.com/id/1383122126/vector/vector-illustration-of-an-abstract-scheme-which-contains-people-icons.jpg?s=612x612&w=0&k=20&c=dELp5546VC94zdiX8i2M1xzs2TNCE_3MKppbFZhduBs=" alt="Connect with others today!" />
+      </div>
+    </>
+  );
+}
 
 function App() {
   return (
@@ -54,7 +86,7 @@ function App() {
             
 
         } />
-
+      <Route path="/" element={<Home />} />
       <Route path="login" element={<Login />} />
       <Route path="createaccount" element={<CreateAccountForm />} />
       <Route path="form-builder" element={<FormBuilder />} /> 
